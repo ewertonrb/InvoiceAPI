982408bf1a58e75e4d32ad4dc224d3c3366d5a97
Ewerton Bonfim <ewerton_tnt@hotmail.com>
2026-08-06 15:21:52 +1000

    chore(persistence): mark user repository explicitly

--- src/main/java/com/invoice/invoice_api/repository/AppUserRepository.java
+++ src/main/java/com/invoice/invoice_api/repository/AppUserRepository.java
@@ -2,10 +2,13 @@ package com.invoice.invoice_api.repository;
 
 import com.invoice.invoice_api.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
     Optional<AppUser> findByEmailIgnoreCase(String email);
 
     boolean existsByEmailIgnoreCase(String email);
